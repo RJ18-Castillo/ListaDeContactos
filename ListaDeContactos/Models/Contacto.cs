4ea909c1cb1d1bfcd123c38d997139674dab212b
RJ18-Castillo <RJ18-Castillo@example.com>
2026-05-22 18:17:40 -0600

    Model Contacto

---
+++ ListaDeContactos/Models/Contacto.cs
@@ -0,0 +1,11 @@
+﻿namespace ListaDeContactos.Api.Models
+{
+    public class Contacto
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public int Numero { get; set; }
+    }
+}
