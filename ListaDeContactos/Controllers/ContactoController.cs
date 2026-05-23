8632de7c9bf68d71a78e01e6cbd0e1fac63dd770
RJ18-Castillo <RJ18-Castillo@example.com>
2026-05-22 18:27:03 -0600

    Controlador

---
+++ ListaDeContactos/Controllers/ContactoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ListaDeContactos.Api.Data;
+using ListaDeContactos.Api.Models;
+
+namespace ListaDeContactos.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactoController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ContactoController(
+            AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Contacto>>> Get()
+        {
+            return await _context.Contactos.ToListAsync();
+        }
+    }
+}
