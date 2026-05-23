9e1ef8e4b8f09cb2448a7a4c0ea3001281361456
RJ18-Castillo <RJ18-Castillo@example.com>
2026-05-22 18:22:01 -0600

    Conexión con BD

---
+++ ListaDeContactos/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ListaDeContactos.Api.Models;
+
+namespace ListaDeContactos.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(
+            DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Contacto> Contactos { get; set; }
+    }
+}
